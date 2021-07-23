0fd8e3b3234f5a3d621d972d65766f52f0eed27d
Antonio Torres <jantonio.torresfi@gmail.com>
2021-07-23 00:54:00 -0400

    Primera version de aes

--- aes-ecb.py
+++ aes-ecb.py
@@ -1,12 +1,25 @@
+import base64
 from Crypto.Cipher import AES
- 
-key = 'urirociojoseomarurirociojoseomar'
- 
-cipher = AES.new(key, AES.MODE_ECB)
-msg =cipher.encrypt('Criptografia 2021-2')
-print (type(msg))
- 
-print(msg.encode("hex"))
- 
-decipher = AES.new(key, AES.MODE_ECB)
-print(decipher.decrypt(msg))
+from Crypto.Util.Padding import pad
+from Crypto.Util.Padding import unpad
+from time import time
+
+key = '0000000000000000000000000000000000000000000000000000000000000000'
+key = bytes.fromhex(key)
+text = '014730f80ac625fe84f026c60bfd547d'
+text = bytes.fromhex(text)
+
+aes = AES.new(key, AES.MODE_ECB)
+# Aes Cifrado
+tiempo_inicial = time()
+encrypt_aes = aes.encrypt(pad(text,AES.block_size))
+# Impresi[on de Mensaje cifrado 
+tiempo_final = time()
+print(encrypt_aes.hex())
+print("Tiempo de ejecución de cifrado: ",(tiempo_final-tiempo_inicial),"segundos")
+
+tiempo_inicial = time()
+msg = unpad(aes.decrypt(encrypt_aes),AES.block_size)
+tiempo_final = time()
+print(msg.hex())
+print("Tiempo de ejecución de descifrado: ",(tiempo_final-tiempo_inicial),"segundos")

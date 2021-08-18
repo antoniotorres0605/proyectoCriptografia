fd6561928cbf852324b8206e749dce587ba58e9a
Antonio Torres <snatchedartist@gmail.com>
2021-08-18 16:45:21 -0500

    Corrección en el del cbc

--- aes-cbc.py
+++ aes-cbc.py
@@ -9,18 +9,30 @@ from Crypto.Random import get_random_bytes
 
 #CIFRADO
 
-data = b"secret"
-key = get_random_bytes(16)
-cipher = AES.new(key, AES.MODE_CBC)
+data = "014730f80ac625fe84f026c60bfd547d"
+data = bytes.fromhex(data)
+IV = "00000000000000000000000000000000"
+IV = bytes.fromhex(IV)
+key = "0000000000000000000000000000000000000000000000000000000000000000"
+key = bytes.fromhex(key)
+
+cipher = AES.new(key, AES.MODE_CBC, iv=IV)
+
 ct_bytes = cipher.encrypt(pad(data, AES.block_size))
 iv = b64encode(cipher.iv).decode('utf-8')
 ct = b64encode(ct_bytes).decode('utf-8')
+
 result = json.dumps({'iv':iv, 'ciphertext':ct})
-print(result)
+
+print(ct)
 
 b64 = json.loads(result)
 iv = b64decode(b64['iv'])
 ct = b64decode(b64['ciphertext'])
 cipher = AES.new(key, AES.MODE_CBC, iv)
+
+
 pt = unpad(cipher.decrypt(ct), AES.block_size)
-print("The message was: ", pt)
+
+
+print("The message was: ", pt.hex())

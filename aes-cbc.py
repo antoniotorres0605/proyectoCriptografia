971e1401a1b65ae0004e15bc76df51a55b4cb6f8
Antonio Torres <snatchedartist@gmail.com>
2021-08-18 21:41:22 -0500

    AES ECB y CBC Completo

--- aes-cbc.py
+++ aes-cbc.py
@@ -1,4 +1,12 @@
-# It is a mode of operation where each plaintext block gets XOR-ed with the previous ciphertext block prior to encryption.
+# It is a mode of operation where each plaintext block gets XOR-ed with 
+# the previous ciphertext block prior to encryption.
+# -------------------------------------------------
+# AES 
+# 1024 bits
+# Modo CBC
+# -------------------------------------------------
+
+# --- BIBLIOTECAS NECESARIAS --
 import json
 from base64 import b64encode
 from base64 import b64decode
@@ -6,33 +14,62 @@ from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
 from Crypto.Util.Padding import unpad
 from Crypto.Random import get_random_bytes
+from timeit import default_timer
 
-#CIFRADO
+# Generación de DATOS, Vector de Inicialización y llave con base al NIST
 
 data = "014730f80ac625fe84f026c60bfd547d"
 data = bytes.fromhex(data)
+
 IV = "00000000000000000000000000000000"
 IV = bytes.fromhex(IV)
+
 key = "0000000000000000000000000000000000000000000000000000000000000000"
 key = bytes.fromhex(key)
 
+# Objeto de cifrado y descifrado
 cipher = AES.new(key, AES.MODE_CBC, iv=IV)
 
+#######################################################
+####################### CIFRADO #######################
+#######################################################
+
+# Se toma el instante 1 del tiempo
+t0 = default_timer()
+
+# Proceso de cifrado
 ct_bytes = cipher.encrypt(pad(data, AES.block_size))
+
+# Se toma el instante 2 del tiempo
+t1 = default_timer()
+
+# Codificación del vector de inicialización
 iv = b64encode(cipher.iv).decode('utf-8')
+
+# Codificación de mensaje cifrado
 ct = b64encode(ct_bytes).decode('utf-8')
 
-result = json.dumps({'iv':iv, 'ciphertext':ct})
+# Resultado
+print("{0:0.10f}".format(t1-t0))
+#print(ct)
 
-print(ct)
+########################################################
+###################### DESCIFRADO ######################
+########################################################
 
-b64 = json.loads(result)
-iv = b64decode(b64['iv'])
-ct = b64decode(b64['ciphertext'])
-cipher = AES.new(key, AES.MODE_CBC, iv)
+# Se toma el instante 1 del tiempo
+t0 = default_timer()
+
+# Objeto para el decodificado 
+cipher = AES.new(key, AES.MODE_CBC, iv=IV)
+
+# Se toma el instante 2 del tiempo
+t1 = default_timer()
+
+# Proceso de descifrado
+pt = unpad(cipher.decrypt(ct_bytes), AES.block_size)
 
 
-pt = unpad(cipher.decrypt(ct), AES.block_size)
+print("{0:0.10f}".format(t1-t0))
 
-
-print("The message was: ", pt.hex())
+#print(pt.hex())
--- aes-ecb.py
+++ aes-ecb.py
@@ -1,3 +1,11 @@
+# -------------------------------------------------
+# AES 
+# 1024 bits
+# Modo ECB
+# -------------------------------------------------
+
+# --- BIBLIOTECAS NECESARIAS --
+# Padding y formateo de datos necesarios para el cifrado
 import base64
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
@@ -5,23 +13,44 @@ from Crypto.Util.Padding import unpad
 from time import time
 from timeit import default_timer
 
-key = '0000000000000000000000000000000000000000000000000000000000000000'
+# Llave de 256 bits
+key = "0000000000000000000000000000000000000000000000000000000000000000"
 key = bytes.fromhex(key)
-text = '014730f80ac625fe84f026c60bfd547d'
+
+# Mensaje en claro
+text = "014730f80ac625fe84f026c60bfd547d"
 text = bytes.fromhex(text)
 
+# Objeto de cifrado y descifrado
 aes = AES.new(key, AES.MODE_ECB)
-# Aes Cifrado
+
+#######################################################
+####################### CIFRADO #######################
+#######################################################
+
+# Se toma el instante 1 del tiempo
 t0 = default_timer()
+
+# Proceso de cifrado
 encrypt_aes = aes.encrypt(pad(text,AES.block_size))
-# Impresion de Mensaje cifrado 
+
+# Se toma el instante 2 del tiempo
 t1 = default_timer()
 #print(encrypt_aes.hex())
 print("{0:0.10f}".format(t1-t0))
-#print("Tiempo de ejecución de cifrado: ",(tiempo_final-tiempo_inicial),"segundos")
 
+
+########################################################
+###################### DESCIFRADO ######################
+########################################################
+
+# Se toma el instante 1 del tiempo
 t0 = default_timer()
+
+# Proceso de descifrado
 msg = unpad(aes.decrypt(encrypt_aes),AES.block_size)
+
+# Se toma el instante 1 del tiempo
 t1 = default_timer()
 #print(msg.hex())
 print("{0:0.10f}".format(t1-t0))
